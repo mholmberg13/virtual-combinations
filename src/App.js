7db329a00a55cf5ce733335c3869920bb376686e
Matt Holmberg <mholmberg13@gmail.com>
2020-09-13 23:09:12 -0600

    Added click event to trigger new combinations

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
 import React from 'react';
 import logo from './logo.svg';
+import Header from './components/Header.js'
+import Main from './components/Main.js'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  state = {
+    strike_one: "",
+    strike_two: "",
+    strike_three: ""
+  }
+
+
+  handleNewCombo = () => {
+    let strikes = ["jab", "cross", "hook", "overhand", "upper left", "upper right", "body left", "body right"]
+    this.setState({
+      strike_one: strikes[Math.floor(Math.random() * strikes.length)],
+      strike_two: strikes[Math.floor(Math.random() * strikes.length)],
+      strike_three: strikes[Math.floor(Math.random() * strikes.length)]
+    })
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <Header></Header>
+        <Main></Main>
+        <h2 onClick={this.handleNewCombo}>New Combo</h2>
+        <h3>{this.state.strike_one}</h3>
+        <h3>{this.state.strike_two}</h3>
+        <h3>{this.state.strike_three}</h3>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/ComboBox.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+class ComboBox extends React.Component {
+    state = {
+        boxing: ["jab", "cross", "hook", "overhand", "upper left", "upper right", "body left", "body right"],
+        kicks: ["leg left", "leg right", "left high", "right high"]
+    }
+
+     
+
+    render () {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default ComboBox;
