43fc400ad633ad0cd9b5961109790e84e3cd7f38
Matt Holmberg <mholmberg13@gmail.com>
2020-09-15 00:14:29 -0600

    Added checkboxes and stop button functionality

--- src/App.js
+++ src/App.js
@@ -9,7 +9,8 @@ class App extends React.Component {
   state = {
     strike_one: "",
     strike_two: "",
-    strike_three: ""
+    strike_three: "",
+    strike_four: ""
   }
 
 
@@ -24,7 +25,11 @@ class App extends React.Component {
 
   handleStart = () => {
     this.handleNewCombo()
-    setInterval(this.handleNewCombo, 5000)
+    this.intervalId = setInterval(this.handleNewCombo, 4000)
+  }
+
+  handleStop = () => {
+    clearInterval(this.intervalId)
   }
 
   render(){
@@ -32,7 +37,14 @@ class App extends React.Component {
       <div className="App">
         <Header></Header>
         <Main></Main>
-        <h2 onClick={this.handleStart}>New Combo</h2>
+        <input type="checkbox" id="select3" name="select3" value="3 Strike Combos"/>
+        <label for="select3">3 Strike Combos</label><br></br>
+        <input type="checkbox" id="select4" name="select4" value="4 Strike Combos"/>
+        <label for="select4">4 Strike Combos</label><br></br>
+        <input type="checkbox" id="select5" name="select5" value="4 Strike Combos"/>
+        <label for="select5">5 Strike Combos</label><br></br>
+        <h2 onClick={this.handleStart}>START</h2>
+        <h2 onClick={this.handleStop}>STOP</h2>
         <h3>{this.state.strike_one}</h3>
         <h3>{this.state.strike_two}</h3>
         <h3>{this.state.strike_three}</h3>
