91abcbf17fe5631874b22ebcc96d2b9745b308d1
Matt Holmberg <mholmberg13@gmail.com>
2020-09-15 22:05:32 -0600

    added state change for checkboxes

--- src/App.js
+++ src/App.js
@@ -10,7 +10,10 @@ class App extends React.Component {
     strike_one: "",
     strike_two: "",
     strike_three: "",
-    strike_four: ""
+    strike_four: "",
+    three_checked: false,
+    four_checked: false,
+    five_checked: false
   }
 
 
@@ -32,16 +35,58 @@ class App extends React.Component {
     clearInterval(this.intervalId)
   }
 
+  toggleThreeCheck = () => {
+    if (this.state.three_checked === false) {
+      this.setState({
+        three_checked: true
+      })
+    }
+    if (this.state.three_checked === true) {
+      this.setState({
+        three_checked: false
+      })
+    }
+    console.log(this.state.three_checked)
+  }
+
+  toggleFourCheck = () => {
+    if (this.state.four_checked === false) {
+      this.setState({
+        four_checked: true
+      })
+    }
+    if (this.state.four_checked === true) {
+      this.setState({
+        four_checked: false
+      })
+    }
+    console.log(this.state.four_checked)
+  }
+
+  toggleFiveCheck = () => {
+    if (this.state.five_checked === false) {
+      this.setState({
+        five_checked: true
+      })
+    }
+    if (this.state.five_checked === true) {
+      this.setState({
+        five_checked: false
+      })
+    }
+    console.log(this.state.five_checked)
+  }
+
   render(){
     return (
       <div className="App">
         <Header></Header>
         <Main></Main>
-        <input type="checkbox" id="select3" name="select3" value="3 Strike Combos"/>
+        <input type="checkbox" id="select3" name="select3" value="3 Strike Combos" onChange={this.toggleThreeCheck}/>
         <label for="select3">3 Strike Combos</label><br></br>
-        <input type="checkbox" id="select4" name="select4" value="4 Strike Combos"/>
+        <input type="checkbox" id="select4" name="select4" value="4 Strike Combos" onChange={this.toggleFourCheck}/>
         <label for="select4">4 Strike Combos</label><br></br>
-        <input type="checkbox" id="select5" name="select5" value="4 Strike Combos"/>
+        <input type="checkbox" id="select5" name="select5" value="4 Strike Combos" onChange={this.toggleFiveCheck}/>
         <label for="select5">5 Strike Combos</label><br></br>
         <h2 onClick={this.handleStart}>START</h2>
         <h2 onClick={this.handleStop}>STOP</h2>
